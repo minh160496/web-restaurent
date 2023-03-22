93fe627fc2dd08218d3c3e71432b1339b7a90a23
minhle <leminh16041996@gmail.com>
2023-03-22 16:30:00 +0700

    addContinuteCheckout

--- src/Page/CheckOut/CheckOut.js
+++ src/Page/CheckOut/CheckOut.js
@@ -5,13 +5,15 @@ import { Container } from "react-bootstrap";
 import LayoutDefault from "Layout/LayoutDefault";
 import CheckOutForm from "./CheckOutForm";
 
+import { pathObj } from "Routers";
+
 import styles from "./CheckOut.module.scss";
 
 const cl = classNames.bind(styles);
 export default function CheckOut() {
   return (
     <div className={cl("checkout")}>
-      <LayoutDefault>
+      <LayoutDefault path={pathObj.checkOut.path}>
         <Container>
           <main>
             <CheckOutForm />
